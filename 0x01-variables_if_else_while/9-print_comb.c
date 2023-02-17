7fb99089f9df56006669b1a769528f2ad4584510
root <amanuel>
2023-02-17 08:48:08 -0800

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,15 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
+	putchar(num);
+	if (num != 9)
+	{	
+	putchar(',');
+	putchar(' ');
 	}
-
+	}
 	putchar('\n');
 
 	return (0);
