b4786e7bd9cf64b643e74308b557d006cb382be6
amanuel <amanuel.demirew@yahoo.com>
2023-03-06 07:14:44 -0800

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,44 @@
 #include "main.h"
 
+/**
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ *
+ * Return: return count
+ **/
 
 unsigned int _strspn(char *s, char *accept)
+
 {
-	unsigned int i, z, count, checker;
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
 
 	i = 0;
-	z = 0;
-	checker = 0;
-	count = 0;
-	while (s[i] != '\0')
+	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
-		z = 0;
-		checker = 0;
-		while (accept[z] != '\0')
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
 		{
-			if (accept[z] == s[i])
+			if (str2[j] == str1[i]) /*Evaluate condition*/
 			{
-				count++;
-				checker = 1;
-			/* Break to early increase efficiency */
+				count++; /*count number*/
 				break;
 			}
-			z++;
+
+			j++;    /*add j+1*/
+		}
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
 		}
 
-		if (checker == 0)
-			return (count);
-		i++;
+		i++; /*add x+1*/
 	}
-	return (count);
+
+	return (count); /*return the value of count*/
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,16 +1,27 @@
 #include "main.h"
 
-
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ *
+ *Return: void
+ */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, b;
+	/*Declaring variables*/
+	int i, j;
 
-	for (i = 0 ; i < 8 ; i++)
+	i = 0;
+	while (i < 8) /*number repetitions*/
 	{
-		for (b = 0 ; b < 8 ; b++)
+		j = 0;
+		while (j < 8)
 		{
-			_putchar(*((*a + i * 8) + b));
+			_putchar (a[i][j]);
+			j++;
 		}
-		_putchar('\n');
+		_putchar ('\n'); /*print new line*/
+		i++; /*add +1*/
 	}
+
 }
