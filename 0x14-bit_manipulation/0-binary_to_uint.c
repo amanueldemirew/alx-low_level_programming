756eb26b8a02098279427e88c3d54dd9c3fd86d6
Amanuel <amanuel.demirew@yahoo.com>
2023-07-06 23:29:28 -0700

    trial 9

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,26 +12,20 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num, i;
 
 	if (!b || !*b)
-	{
 		return (0);
-	}
 
 	num = i = 0;
 	while (b[i])
 	{
 		if (b[i] > 49)
-		{
 			return (0);
-		}
 		else if (b[i] == 49)
 		{
 			num <<= 1;
 			num += 1;
 		}
 		else
-		{
 			num <<= 1;
-		}
 		i++;
 	}
 	return (num);
