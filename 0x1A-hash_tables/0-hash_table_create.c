3a01b1200c695bd8d8fe9d96498d1071b4f40242
Amanuel <amanuel.demirew@yahoo.com>
2023-09-14 13:47:15 +0300

    2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		fprintf(stderr, "Error:malloc faild");
+		return (NULL);
+	}
 	ht->size = size;
 	ht->array = malloc(sizeof(ht->array) * size);
-
+	if (ht == NULL)
+	{
+		fprintf(stderr, "Error:malloc faild");
+		return (NULL);
+	}
 	return (ht);
 }
